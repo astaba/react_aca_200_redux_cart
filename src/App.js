3521b2438317c14c7f615c690651f4910fa30487
Astar <bahouidi99@gmail.com>
2023-08-18 09:58:26 +0100

    useReducer for Http state and feedback
    It is perfectly fine to apply either useReducer or useState to this kind of local state management, however since we already define a Redux UI slice we should use it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import { useEffect } from "react";
+import { useEffect, useReducer, Fragment } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+
+const putReducer = (state, action) => {
+  switch (action.type) {
+    case "PUT_INIT":
+      return {
+        status: "pending",
+        title: "Is pending...",
+        message: "The PUT request was launched",
+      };
+    case "PUT_SUCCEED":
+      return {
+        status: "success",
+        title: "Success!",
+        message: "Cart successfully updated!",
+      };
+    case "PUT_FAILED":
+      return {
+        status: "error",
+        title: "Failed!",
+        message: action.payload,
+      };
+    default:
+      throw new Error(`Unexpected action type: ${action.type}`);
+  }
+};
 
 function App() {
   const isCartDisplayed = useSelector((state) => state.uiState.isCartDisplayed);
   const cart = useSelector((state) => state.cartState);
+  const [putRequestState, putDispatch] = useReducer(putReducer, {
+    status: "",
+    title: "",
+    message: "",
+  });
 
   useEffect(() => {
-    fetch("https://max-react-20-redux-default-rtdb.firebaseio.com/cart.json", {
-      method: "PUT",
-      body: JSON.stringify(cart),
-    });
+    const launchPut = async () => {
+      putDispatch({ type: "PUT_INIT" });
+      try {
+        const response = await fetch(
+          "https://max-react-20-redux-default-rtdb.firebaseio.com/cart.json",
+          {
+            method: "PUT",
+            body: JSON.stringify(cart),
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Something went wrong");
+        }
+        putDispatch({ type: "PUT_SUCCEED" });
+      } catch (error) {
+        putDispatch({ type: "PUT_FAILED", payload: error.message });
+      }
+    };
+
+    launchPut();
   }, [cart]);
 
   return (
-    <Layout>
-      {isCartDisplayed && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      <Notification
+        status={putRequestState.status}
+        title={putRequestState.title}
+        message={putRequestState.message}
+      />
+      <Layout>
+        {isCartDisplayed && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
